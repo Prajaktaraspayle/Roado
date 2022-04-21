8c67db7aeeb1a7c3fa19c5f25dcbe2de48a0512a
Jishan Pathan <jishanpatel78@gmail.com>
2022-04-21 10:06:12 +0530

    commit

--- server/utils/helper.js
+++ server/utils/helper.js
@@ -1,63 +1,51 @@
 const axios = require('axios')
 require('dotenv').config()
-const http = require('http')
+//const http = require('http')
 //app key and app id provided by oxford
 const app_id = process.env.oxfordAppId
 const app_key = process.env.oxfordAppKey
-const fields = "definitions"
+const fields = 'definitions,examples,pronunciations,etymologies,variantForms'
 const strictMatch = "false";
-const wordId = "ace"
-//oxford API base url
 oxfordBaseUrl = 'https://od-api.oxforddictionaries.com/api/v2/'
+const word = 'rose'
+//oxford API base url
 
-//function to get details of words
-const getWordDetail = async () => {
+async function getWordDetail() {
     const options = {
-        host: 'od-api.oxforddictionaries.com',
-        port : 443,
-        path: `/api/v2/entries/en-us/${wordId}?fields=${fields}&strictMatch=${strictMatch}`,
         method: 'GET',
+        url: oxfordBaseUrl + 'entries/en-us/' + word + '?fields=' + fields + '&strictMatch=' + strictMatch,
         headers: {
             'app_id': app_id,
             'app_key': app_key
         }
-    }
-
-    http.get(options, (res) => {
-        let body = ''
-        res.on('data', (chunk) => {
-            body += chunk
-        })
-        res.on('end', () => {
-            let parsed = JSON.stringify(body)
-            console.log(parsed)
-        })
-    })
+    };
 
     //definig an empty object
     let wordObj = {}
 
     //error handling
     try {
-        let { data } = await axios(options)
-
-        //add required fields to word object
-        wordObj.word = data.word
-        wordObj.entries = []
-        data.results[0].lexicalEntries.forEach(entry => {
+        //parsing the response
+        let {data} = await axios(options)
+        wordObj.word = data.results[0].word
+        wordObj.entries = [];
+        //parsing the entries
+        data.results[0].lexicalEntries.forEach(lexicalEntry => {
             wordObj.entries.push({
-                partOfSpeech: entry.lexicalCategory.text,
-                origin: entry.entries[0].etymologies,
-                definitions: entry.entries[0].senses[0].definitions,
-                examples: entry.entries[0].senses[0].examples.map(example => example.text)
+                partOfSpeech: lexicalEntry.lexicalCategory.text,
+                origin: lexicalEntry.entries[0].etymologies,
+                definitions: lexicalEntry.entries[0].senses[0].definitions,
+                examples: lexicalEntry.entries[0].senses[0].examples.map(example => example.text)
             })
+        
         })
-
-        //return word object
+        console.log(wordObj)
+        //returning the word object
         return wordObj
-    } catch (e) {
+    } catch (error) {
+        console.log(error)
         return false
     }
 }
 
-module.exports = getWordDetail
+module.exports = getWordDetail;
